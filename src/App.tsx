e88ba2fd0761dd9f129154775a8f223a05e89ebe
Marcus Hudnell <hudnell.marcus@gmail.com>
2024-06-06 10:19:04 -0700

    fixed background image sizing

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,25 @@ import "./styles.css";
 import Header from "./Components/Header";
 import Options from "./Components/Options";
 import RecipeCard from "./Components/Recipe";
-import { Recipe } from "../types"; 
+import { Recipe } from "../types";
 
 function App() {
   const [diet, setDiet] = useState<string>("");
-    const [recipe, setRecipe] = useState<Recipe | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [currentRecipe, setCurrentRecipe] = useState<Recipe | null>(null); 
- // let recipeArray=[""]; 
+  const [currentRecipe, setCurrentRecipe] = useState<Recipe | null>(null);
+  // let recipeArray=[""];
 
   const handleNewRecipe = (newRecipe: Recipe) => {
     setCurrentRecipe(newRecipe);
 
-    localStorage.setItem('currentRecipe', JSON.stringify(newRecipe))
+    localStorage.setItem("currentRecipe", JSON.stringify(newRecipe));
   };
 
   const handleReset = () => {
-    setCurrentRecipe(null)
+    setCurrentRecipe(null);
 
-    localStorage.removeItem('currentRecipe')
-  }; 
+    localStorage.removeItem("currentRecipe");
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -41,40 +40,44 @@ function App() {
         throw new Error(`Failed to fetch recipes: ${response.statusText}`);
       }
       const data = await response.json();
-      handleNewRecipe(data.recipe); 
-    
+      handleNewRecipe(data.recipe);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    if (recipe) {
-      localStorage.setItem('currentRecipe', JSON.stringify(recipe));
+    if (currentRecipe) {
+      localStorage.setItem("currentRecipe", JSON.stringify(currentRecipe));
     }
-  }, [recipe]);
-  
+  }, [currentRecipe]);
+
   useEffect(() => {
-    const savedRecipe = localStorage.getItem('currentRecipe');
+    const savedRecipe = localStorage.getItem("currentRecipe");
 
     if (savedRecipe) {
-      setCurrentRecipe(JSON.parse(savedRecipe)); 
+      setCurrentRecipe(JSON.parse(savedRecipe));
     }
-  }, []); 
+  }, []);
 
   return (
-    <div className="flex flex-col background h-screen items-center justify-center">
-      <div className="flex flex-col h-[95%] w-[65%] bg-white rounded-md m-14">
-        <Header />
-        <Options diet={diet} setDiet={setDiet} handleSubmit={handleSubmit} handleReset={handleReset}/>
-        
-        {loading || !currentRecipe ? (
-          <div>Loading...</div>
-        ) : (
-          currentRecipe &&
+    <div className="flex flex-col min-h-screen background">
+      <div className="flex flex-col items-center py-8">
+        <div className="flex flex-col h-[95%] w-[65%] bg-white rounded-md m-14">
+          <Header />
+          <Options
+            diet={diet}
+            setDiet={setDiet}
+            handleSubmit={handleSubmit}
+            handleReset={handleReset}
+          />
 
-          <RecipeCard recipe={currentRecipe}  />
-        )}
+          {loading || !currentRecipe ? (
+            <div>Loading...</div>
+          ) : (
+            currentRecipe && <RecipeCard recipe={currentRecipe} />
+          )}
+        </div>
       </div>
     </div>
   );
